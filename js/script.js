73371d769fec07945ac1250307cdd96de4a54711
Egor <makarov.e1999@yandex.ru>
2018-12-21 23:06:48 +0300

    Final version
    Second step to finish project

--- js/script.js
+++ js/script.js
@@ -1,47 +1,66 @@
-var sliders=document.getElementsByClassName('main-slider-item');
-var buttons=document.getElementsByClassName('main-slider-dot');//Кнопкип
-var openButton=document.querySelector('.write-us-button');
-var modal=document.querySelector('.modal-feedback');
-var closeButton=document.querySelector('.close-form-button');
-var loginField=document.getElementById('username');
-var emailField=document.getElementById('useremail');
-var commentField=document.getElementById('comment');
+var sliders = document.getElementsByClassName ('main-slider-item');
+var buttons = document.getElementsByClassName ('main-slider-dot');
+var openButton = document.querySelector ('.write-us-button');
+var modal = document.querySelector ('.modal-feedback');
+var closeButton = document.querySelector ('.close-form-button');
+var loginField = document.getElementById ('username');
+var emailField = document.getElementById ('useremail');
+var commentField = document.getElementById ('comment');
+var sendButton = document.querySelector ('.send-form-button');
 
-console.log(openButton);
-console.log(modal);
-buttons[0].onclick=function(){
-  for (var i = 1; i <buttons.length; i++) {
-  if(buttons[i].classList.contains('active-dot')) {
-      buttons[i].classList.remove('active-dot');
-      sliders[i].classList.remove('active');
+buttons[0].onclick = function () {
+  for (var i = 1; i < buttons.length; i++) {
+    if (buttons[i].classList.contains ('active-dot')) {
+        buttons[i].classList.remove ('active-dot');
+        sliders[i].classList.remove ('active');
     }
   }
-  buttons[0].classList.add('active-dot');
-  sliders[0].classList.add('active');
+  buttons[0].classList.add ('active-dot');
+  sliders[0].classList.add ('active');
 }
-buttons[1].onclick=function(){
-  for (var i = 0; i <buttons.length; i++) {
-  if(buttons[i].classList.contains('active-dot')) {
-      buttons[i].classList.remove('active-dot');
-      sliders[i].classList.remove('active');
+
+buttons[1].onclick = function () {
+  for (var i = 0; i < buttons.length; i++) {
+    if (buttons[i].classList.contains ('active-dot')) {
+        buttons[i].classList.remove ('active-dot');
+        sliders[i].classList.remove ('active');
     }
   }
-  buttons[1].classList.add('active-dot');
-  sliders[1].classList.add('active');
+  buttons[1].classList.add ('active-dot');
+  sliders[1].classList.add ('active');
 }
-buttons[2].onclick=function(){
-  for (var i = 0; i <buttons.length; i++) {
-  if(buttons[i].classList.contains('active-dot')) {
-      buttons[i].classList.remove('active-dot');
-      sliders[i].classList.remove('active');
+
+buttons[2].onclick = function () {
+  for (var i = 0; i < buttons.length; i++) {
+    if(buttons[i].classList.contains ('active-dot')) {
+        buttons[i].classList.remove ('active-dot');
+        sliders[i].classList.remove ('active');
     }
   }
-  buttons[2].classList.add('active-dot');
-  sliders[2].classList.add('active');
+  buttons[2].classList.add ('active-dot');
+  sliders[2].classList.add ('active');
 }
-openButton.addEventListener('click',function() {
-  modal.classList.add('active-modal');
+
+openButton.addEventListener ('click', function () {
+  modal.classList.add ('active-modal');
 });
-closeButton.addEventListener('click',function(){
-  modal.classList.remove('active-modal');
+
+closeButton.addEventListener ('click', function () {
+  modal.classList.remove ('active-modal');
+});
+
+sendButton.addEventListener ('click', function (event) {
+  if (!loginField.value || !emailField.value || !commentField.value) {
+    event.preventDefault ();
+    modal.classList.add ('invalid-form');
+  }
+  if (!loginField.value) {
+    loginField.classList.add ('invalid-field');
+  }
+  if (!emailField.value) {
+    emailField.classList.add ('invalid-field');
+  }
+  if (!commentField.value) {
+    commentField.classList.add ('invalid-field');
+  }
 });
