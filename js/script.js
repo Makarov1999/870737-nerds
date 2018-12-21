c1b4ddab13abe0f2d3a5fff835505989604f1d34
Egor <makarov.e1999@yandex.ru>
2018-12-21 19:56:33 +0300

    Final version
    First step to finish project

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 var sliders=document.getElementsByClassName('main-slider-item');
-var buttons=document.getElementsByClassName('main-slider-dot')
+var buttons=document.getElementsByClassName('main-slider-dot');//Кнопкип
+var openButton=document.querySelector('.write-us-button');
+var modal=document.querySelector('.modal-feedback');
+var closeButton=document.querySelector('.close-form-button');
+var loginField=document.getElementById('username');
+var emailField=document.getElementById('useremail');
+var commentField=document.getElementById('comment');
+
+console.log(openButton);
+console.log(modal);
 buttons[0].onclick=function(){
   for (var i = 1; i <buttons.length; i++) {
-  if(buttons[i].classList.contains('active-dot')){
-    buttons[i].classList.remove('active-dot');
-    sliders[i].classList.remove('active');
+  if(buttons[i].classList.contains('active-dot')) {
+      buttons[i].classList.remove('active-dot');
+      sliders[i].classList.remove('active');
     }
   }
   buttons[0].classList.add('active-dot');
@@ -12,9 +21,9 @@ buttons[0].onclick=function(){
 }
 buttons[1].onclick=function(){
   for (var i = 0; i <buttons.length; i++) {
-  if(buttons[i].classList.contains('active-dot')){
-    buttons[i].classList.remove('active-dot');
-    sliders[i].classList.remove('active');
+  if(buttons[i].classList.contains('active-dot')) {
+      buttons[i].classList.remove('active-dot');
+      sliders[i].classList.remove('active');
     }
   }
   buttons[1].classList.add('active-dot');
@@ -22,11 +31,17 @@ buttons[1].onclick=function(){
 }
 buttons[2].onclick=function(){
   for (var i = 0; i <buttons.length; i++) {
-  if(buttons[i].classList.contains('active-dot')){
-    buttons[i].classList.remove('active-dot');
-    sliders[i].classList.remove('active');
+  if(buttons[i].classList.contains('active-dot')) {
+      buttons[i].classList.remove('active-dot');
+      sliders[i].classList.remove('active');
     }
   }
   buttons[2].classList.add('active-dot');
   sliders[2].classList.add('active');
 }
+openButton.addEventListener('click',function() {
+  modal.classList.add('active-modal');
+});
+closeButton.addEventListener('click',function(){
+  modal.classList.remove('active-modal');
+});
